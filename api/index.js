f4c4df7527ac4397c09b32b648e7b5a76c70c329
Santosh <santosh.mutyala@uni-oldenburg.de>
2025-04-05 14:50:20 +0200

    add cors

--- api/index.js
+++ api/index.js
@@ -1,6 +1,9 @@
 const express = require("express");
 const app = express();
 const port = process.env.PORT || 3000;
+const cors = require("cors");
+
+app.use(cors());
 
 app.get("/", (req, res) => {
     res.status(200).json({
