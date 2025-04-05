1e2b047ae2bf48be68fb3acad8d7a06c54945fd6
Santosh <santosh.mutyala@uni-oldenburg.de>
2025-04-05 14:25:14 +0200

    init api

---
+++ api/index.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.get("/", (req, res) => {
+    res.status(200).json({
+        message: "Hello World"
+    })
+});
+
+app.listen(port, ()=> {
+    console.log(`Server is running on port ${port}`);
+});
