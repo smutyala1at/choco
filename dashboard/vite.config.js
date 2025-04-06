5553907954550cdf818d110ffcdad6f60a477079
smutyala1at <santosh.mutyala.dev@gmail.com>
2025-04-06 13:32:15 +0200

    test tracking page

--- dashboard/vite.config.js
+++ dashboard/vite.config.js
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+import path from 'path'
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -8,8 +9,19 @@ export default defineConfig({
     react(), 
     tailwindcss(),
   ],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
+  server: {
+    open: true,
+  },
   build: {
     outDir: 'dist',
   },
+  // 🔥 This part is key for React Router!
+  base: '/',
 })
+
 
